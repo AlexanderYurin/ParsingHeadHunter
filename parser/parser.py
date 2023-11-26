726da3909166e7b9635ecfafb0c7d205a9728d52
Alexander Yurin <hiimmandarin32@gmail.com>
2023-11-26 20:04:04 +0300

    Добавлена функция get_url_vacancy для получения url  каждой вакансии

--- parser/parser.py
+++ parser/parser.py
@@ -1,23 +1,41 @@
+from typing import List
+
 import aiohttp
 import asyncio
 from bs4 import BeautifulSoup as bs
 
 from connect import get_responce_text
 
+
 params = {"text": "python"}
 
 
-async def get_pages(session, vacancy: str) -> str:
+async def get_pages(session, url) -> int:
 	"""
 	функция для получение последней страницы пагинации
 	:param session:
-	:param vacancy:
+	:param url:
 	:return:
 	"""
-	resp = get_responce_text(session, "https://hh.ru/search/vacancy?text={vacancy}")
+	resp = get_responce_text(session, url)
 	soup = bs(resp, "lxml")
-	pages = soup.find_all()
-	last_page = pages[-1].text
+	data = soup.find_all(class_="bloko-button")
+	last_page = int(data[-2].text)
 	return last_page
+
+
+async def get_url_vacancy(session, url: str) -> List:
+	"""
+	Функция для получения url каждой вакансии
+
+	:param session:
+	:param url:
+	:return:
+	"""
+	resp = get_responce_text(session, url)
+	soup = bs(resp, "lxml")
+	data = soup.find_all(class_="serp-item__title")
+	urls = list(map(lambda x: x.get("href"), data))
+	return urls
 
 
